ab6b83405f9ae34fdd9da92da38fe37fec70fe84
etim uwem <31390962+iamsoughtout@users.noreply.github.com>
2019-11-10 02:31:07 +0100

    error fixed

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 
+import { Product } from '../product';
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
